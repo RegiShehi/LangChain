1bd0b145d6f65fdd97dde06fbe5d0bff1a6836be
Regi Shehi <regi_shehi@hotmail.com>
2024-09-09 23:15:56 +0200

    Chain 2 chains together

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from langchain_openai import OpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import sequential
 from dotenv import load_dotenv
 import argparse
 
 load_dotenv()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--task", default="return a list of positive numbers")
 parser.add_argument("--language", default="python")
 args = parser.parse_args()
 
@@ -18,8 +19,19 @@ code_prompt = PromptTemplate(
     input_variables=["language", "task"],
 )
 
-chain = code_prompt | llm | StrOutputParser()
+test_prompt = PromptTemplate(
+    template="Write a test for the following {language} code:\n{code}",
+    input_variables=["language", "code"],
+)
 
-result = chain.invoke({"language": args.language, "task": args.task})
+code_chain = code_prompt | llm | StrOutputParser()
 
-print(result)
+test_chain = test_prompt | llm | StrOutputParser()
+
+# Invoke the code_chain to get the generated code
+code_result = code_chain.invoke({"language": args.language, "task": args.task})
+print("Generated Code:", code_result)
+
+# Use the generated code as input to the test_chain
+test_result = test_chain.invoke({"language": args.language, "code": code_result})
+print("\nGenerated Test:", test_result)
